b6af9bf9f317784787517bab9a1e0af8d67d3be8
PY-GZKY <341796767@qq.com>
2022-02-15 14:49:32 +0800

    Update file_core.py

--- src/file_core.py
+++ src/file_core.py
@@ -1,6 +1,5 @@
 # -*- coding:utf-8 -*-
 import os
-
 import pandas
 import pymysql
 from colorama import init as colorama_init_
@@ -17,11 +16,6 @@ class FileEngine():
 
     def __init__(self):
         ...
-
-    def csv_to_sql(self, file_path, db_setting):
-        df_csv_ = pandas.read_csv(file_path)
-        conn_ = pymysql.connect(**db_setting)
-        df_csv_.to_sql(name='test_', con=conn_)
 
 
 if __name__ == '__main__':
