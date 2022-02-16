2c9f4c1fdd6888197a49380688e569fad46bacbc
PY-GZKY <341796767@qq.com>
2022-02-16 11:40:15 +0800

    Update cli.py

--- src/cli.py
+++ src/cli.py
@@ -1,7 +1,6 @@
 import click
 from colorama import init
 
-from .file_core import FileEngine
 from .version import __version__
 
 init(autoreset=True)
@@ -9,11 +8,8 @@ init(autoreset=True)
 
 @click.command("fc")
 @click.version_option(__version__, '-V', '--version', prog_name='fc')
-@click.option('--type')
-@click.option('--ord', default=None, help="")
-@click.option('--tar', is_flag=True, help="")
-def cli(*, type: str = '', ord: str, tar: str):
-    f_ = FileEngine()
+def cli():
+    ...
 
 
 if __name__ == '__main__':
