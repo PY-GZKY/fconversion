5f099b27fc2084281888f2f97bb7f00e48198ef0
PY-GZKY <341796767@qq.com>
2022-02-18 14:59:29 +0800

    ...

--- src/file_core.py
+++ src/file_core.py
@@ -1,6 +1,7 @@
 # -*- coding:utf-8 -*-
 from colorama import init as colorama_init_
 from dotenv import load_dotenv
+from moviepy.editor import VideoFileClip
 
 load_dotenv(verbose=True)
 colorama_init_(autoreset=True)
@@ -19,6 +20,18 @@ class FileEngine():
 
     def mp4_to_mp3(self):
         ...
+
+    def flv_to_mp3(self, source_file: str, target_file: str):
+        """
+        :param source_file: .fly
+        :param target_file: .mp3
+        :return:
+        MoviePy - Writing audio in 点弦泛音高能！《江南》美爆的「指弹吉他」！林俊杰听了都想点赞！.mp3
+        MoviePy - Done.
+        """
+        video = VideoFileClip(source_file)
+        audio = video.audio
+        audio.write_audiofile(target_file)
 
     def mp4_to_wav(self):
         ...
