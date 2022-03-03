c5f514105acca977e11a69920be79c1030c345a1
PY-GZKY <341796767@qq.com>
2022-03-03 17:53:39 +0800

    Update constants.py

--- src/constants.py
+++ src/constants.py
@@ -7,26 +7,28 @@ except ModuleNotFoundError as e:
     os.system("pip install pymdown-extensions")
     from pymdownx import superfences
 
-# WKHTMLTOPDF_PATH = 'E:/Py_Word_Code/fconversion/src/bin/wkhtmltopdf.exe'  # 安装位置
-# WKHTMLTOIMAGE_PATH = 'E:/Py_Word_Code/fconversion/src/bin/wkhtmltoimage.exe'  # 安装位置
-WKHTMLTOPDF_PATH = 'D:/wkhtmltopdf/bin/wkhtmltopdf.exe'  # 安装位置
-WKHTMLTOIMAGE_PATH = 'D:/wkhtmltopdf/bin/wkhtmltoimage.exe'  # 安装位置
-PANDAS_ENCODING = 'utf_8_sig'
+# WKHTMLTOPDF_PATH = r'E:\\Py_Word_Code\\fconversion\\src\\bin\\wkhtmltopdf.exe'  # 安装位置
+# WKHTMLTOIMAGE_PATH = r'E:\\Py_Word_Code\\fconversion\\src\\bin\\wkhtmltoimage.exe'  # 安装位置
+WKHTMLTOPDF_PATH = r'D:/wkhtmltopdf/bin/wkhtmltopdf.exe'  # 安装位置
+WKHTMLTOIMAGE_PATH = r'D:/wkhtmltopdf/bin/wkhtmltoimage.exe'  # 安装位置
+ENCODING_ = 'utf_8_sig'
 TIME_ZONE = 'Asia/Shanghai'
 
 # 扩展配置
 MD_EXTENSIONS = [
     'toc',  # 目录，[toc]
     'extra',  # 缩写词、属性列表、释义列表、围栏式代码块、脚注、在HTML的Markdown、表格
-
     'mdx_math',  # KaTeX数学公式，$E=mc^2$和$$E=mc^2$$
     'markdown_checklist.extension',  # checklist，- [ ]和- [x]
-    'markdown.extensions.extra',
-    'markdown.extensions.codehilite',
-    'markdown.extensions.tables',
-    'markdown.extensions.toc'
+    'pymdownx.magiclink',  # 自动转超链接，
+    'pymdownx.caret',  # 上标下标，
+    'pymdownx.superfences',  # 多种块功能允许嵌套，各种图表
+    'pymdownx.betterem',  # 改善强调的处理(粗体和斜体)
+    'pymdownx.mark',  # 亮色突出文本
+    'pymdownx.highlight',  # 高亮显示代码
+    'pymdownx.tasklist',  # 任务列表
+    'pymdownx.tilde',  # 删除线
 ]
-
 
 MD_EXTENSIONS_CONFIGS = {
     'mdx_math': {
