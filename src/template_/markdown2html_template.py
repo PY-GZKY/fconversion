290ee05b46296a9b117cdde4fe09209b8c0032c0
PY-GZKY <341796767@qq.com>
2022-03-03 14:06:45 +0800

    Feat: 新增 HTML 模板

---
+++ src/template_/markdown2html_template.py
@@ -0,0 +1,14 @@
+html_ = '''
+<!DOCTYPE html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, minimal-ui">
+    <title>{title_}</title>
+    <link rel="stylesheet" href="../static_/default.css">
+    <link rel="stylesheet" href="../static_/github.css">
+</head>
+<body for="html-export">
+<div class="mume markdown-preview  ">{div_}</div>
+</body>
+</html>
+'''
