22c34d1af14e57024a48679d45e8492f0951e8d9
PY-GZKY <341796767@qq.com>
2022-03-03 17:54:08 +0800

    Update test_.py

--- tests/test_.py
+++ tests/test_.py
@@ -1,22 +1,30 @@
 import dotenv
 import pytest
 
+from src.func_.html_ import HTML
 from src.func_.markdown_ import MARKDOWN
 
 dotenv.load_dotenv(verbose=True)
 
 
 def setup_function():
-    global M
+    global M, H
     M = MARKDOWN()
+    H = HTML()
 
 
-def test_markdown2pdf():
-    M.markdown2pdf(input_file='./tf_/春回大地.md', output_file='./tf_/春回大地.pdf')
+def test_html2pdf():
+    H.html2pdf(html_file='./tf_/hello_.html')
+
+# def test_html2image():
+#     H.html2image()
+
+# def test_markdown2pdf():
+#     M.markdown2pdf(input_file='./tf_/hello_.md', output_file='./tf_/hello_.pdf')
 
 
 # def test_markdown2html():
-#     M.markdown2html(input_file='./tf_/春回大地.md', output_file='./tf_/春回大地.html')
+#     M.markdown2html(input_file='./tf_/hello_.md', output_file='./tf_/hello_.html')
 
 
 def teardown_function():
