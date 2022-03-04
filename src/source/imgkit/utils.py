0785edf79c6f7d704710f6b7b7d0c235a620dc3c
PY-GZKY <341796767@qq.com>
2022-03-04 18:37:01 +0800

    Update utils.py

--- src/source/imgkit/utils.py
+++ src/source/imgkit/utils.py
@@ -3,18 +3,15 @@ import sys
 
 
 def get_configuration_():
-    if sys.platform == 'win32':
-        wkhtmltopdf_path = "E:/Py_Word_Code/fconversion/src/bin/wkhtmltoimage.exe"
-    else:
-        for find_cmd in ("where", "which"):
-            try:
-                wkhtmltopdf_path = (
-                    subprocess.check_output([find_cmd, "wkhtmltoimage"]).strip().decode("utf-8")
-                )
-                break
-            except subprocess.CalledProcessError:
-                wkhtmltopdf_path = "command not found"
-            except OSError:
-                wkhtmltopdf_path = "command not found"
+    for find_cmd in ("where", "which"):
+        try:
+            wkhtmltopdf_path = (
+                subprocess.check_output([find_cmd, "wkhtmltoimage"]).strip().decode("utf-8")
+            )
+            break
+        except subprocess.CalledProcessError:
+            wkhtmltopdf_path = "command not found"
+        except OSError:
+            wkhtmltopdf_path = "command not found"
 
     return wkhtmltopdf_path
