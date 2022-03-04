13034a1e4d539d574c890e9cf6986ab1c4b13915
PY-GZKY <341796767@qq.com>
2022-03-04 18:37:08 +0800

    Update utils.py

--- src/source/pdfkit/utils.py
+++ src/source/pdfkit/utils.py
@@ -7,7 +7,7 @@ def get_configuration_(configuration):
     configuration_ = None
     if configuration is None:
         if sys.platform == 'win32':
-            wkhtmltopdf_path = "E:/Py_Word_Code/fconversion/src/bin/wkhtmltopdf.exe"
+            wkhtmltopdf_path = ""
             configuration_ = Configuration(wkhtmltopdf=wkhtmltopdf_path)
             print("wkhtmltopdf_path：", configuration_.wkhtmltopdf)
     else:
