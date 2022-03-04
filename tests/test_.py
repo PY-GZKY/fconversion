fb404d972fc3d54bb2278a2d801377d93110bcc7
PY-GZKY <341796767@qq.com>
2022-03-04 18:35:33 +0800

    Update test_.py

--- tests/test_.py
+++ tests/test_.py
@@ -13,18 +13,20 @@ def setup_function():
     H = HTML()
 
 
-def test_html2pdf():
-    H.html2pdf(input_file='./tf_/hello_.html',output_file='./tf_/hello_.pdf')
+# def test_html2pdf():
+#     H.html2pdf(input_path='./tf_/hello_.html',output_path='./tf_/hello_.pdf')
 
-# def test_html2image():
-#     H.html2image()
+def test_html2image():
+    H.html2image(input_path='hello_.html', output_path='hello_.png')
 
 # def test_markdown2pdf():
-#     M.markdown2pdf(input_file='./tf_/hello_.md', output_file='./tf_/hello_.pdf')
+#     M.markdown2pdf(input_path='./tf_/hello_.md', output_path='./tf_/hello_.pdf')
 
+# def test_markdown2image():
+#     M.markdown2image(input_path='./tf_/hello_.md', output_path='./tf_/hello_.jpg')
 
 # def test_markdown2html():
-#     M.markdown2html(input_file='./tf_/hello_.md', output_file='./tf_/hello_.html')
+#     M.markdown2html(input_path='./tf_/hello_.md', output_path='./tf_/hello_.html')
 
 
 def teardown_function():
