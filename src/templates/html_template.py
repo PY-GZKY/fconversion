6cac0d3db4a0376841bde5038343d04879121679
PY-GZKY <341796767@qq.com>
2022-03-06 03:20:16 +0800

    Update html_template.py

--- src/templates/html_template.py
+++ src/templates/html_template.py
@@ -8,10 +8,13 @@ html_ = '''
     <link rel="stylesheet" href="../../src/static/markdown.css">
     <link rel="stylesheet" href="../../src/static/tasklist.css">
     <link rel="stylesheet" href="../../src/static/codehilite.css">
+    <link rel="stylesheet" href="../../src/static/progress.css">
     <script src="https://unpkg.com/mermaid@8.7.0/dist/mermaid.min.js"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex/dist/katex.min.css" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/katex/dist/katex.min.js" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/katex/dist/contrib/mathtex-script-type.min.js" defer></script>
+
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/mathjax@2/MathJax.js"></script>
 </head>
 <body{class_}>
 <div class="mume markdown-preview">            
